c8e234de4ff8d2b96b48a646d5d95b653eed2581
EC2 Default User <ec2-user@ip-172-31-16-17.ap-northeast-1.compute.internal>
2021-01-18 00:59:27 +0000

    generate model & controller post

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layouts')
+
+@section('title', 'Simple Board')
+
+@section('content')
+    @if (session('message'))
+        {{ session('message') }}
+    @endif
+
+    {{ $post->title }}
+    {{ $post->content }}
+@endsection 
